f4a7ff2fd7827118d81de4926ffa759bd1ccb92b
Leon Barrett <lbarrett@climate.com>
2016-01-19 15:44:17 -0800

    Version bump to 1.1.1

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 ;; and limitations under the License.
 
 (defproject com.climate/claypoole
-  "1.1.0-SNAPSHOT"
+  "1.1.1"
   :description "Claypoole: Threadpool tools for Clojure."
   :url "http://github.com/TheClimateCorporation/claypoole/"
   :license {:name "Apache License Version 2.0"
